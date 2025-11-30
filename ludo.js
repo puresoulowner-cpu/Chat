5bacba3a5a0b21ced0fdb85a2be361f6dff3ddd2
puresoulowner-cpu <puresoulowner@gmail.com>
2025-11-30 13:40:50 +0530

    Update ludo.js

--- ludo.js
+++ ludo.js
@@ -49,7 +49,7 @@ function changePlayer() {
     var badtext = document.getElementById('badtext');
     badtext.innerText = "";
     var dice = document.getElementById('dice');
-    dice.style.backgroundImage = "url(Images/dice.gif)";
+    dice.style.backgroundImage = "url(dice.gif)";
 }
 var positions = {
     redpawn1: 0, redpawn2: 0, redpawn3: 0, redpawn4: 0,
@@ -201,7 +201,7 @@ function randomNum() {
     if (!clicked) {
         num = Math.floor((Math.random() * 6) + 1);;
         var dice = document.getElementById('dice');
-        dice.style.backgroundImage = "url(Images/" + num + ".jpg)";
+        dice.style.backgroundImage = "url(" + num + ".jpg)";
         clicked = true;
     }
     if (num != 6&&DontHaveOtherFree()) {
@@ -289,7 +289,7 @@ function randomMove(Color, paw) {
                     num = 0;
                     clicked = false;
                     var dice = document.getElementById('dice');
-                    dice.style.backgroundImage = "url(Images/dice.gif)";
+                    dice.style.backgroundImage = "url(dice.gif)";
                 }
                 else Stuck();
             }
